bf618df6cd3470e04b0451fab124003cb95d003a
ihsanriadi123 <muhammad.ihsan.ft22@mail.umy.ac.id>
2025-01-13 08:43:16 +0700

    untuk viemodel mahasiswa dengan repository nya

--- app/src/main/java/com/example/applicationfirebasefilestore/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/applicationfirebasefilestore/ui/viewmodel/HomeViewModel.kt
@@ -1,93 +1,42 @@
 package com.example.applicationfirebasefilestore.ui.viewmodel
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.applicationfirebasefilestore.Repository.RepositoryMhs
 import com.example.applicationfirebasefilestore.model.Mahasiswa
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.launch
 
-@Composable
-fun MhsCard(
-    mahasiswa: Mahasiswa,
-    modifier: Modifier = Modifier,
-    onDeleteClick: (Mahasiswa) -> Unit = {}
-){
-    Card(
-        modifier = modifier,
-        shape = MaterialTheme.shapes.medium,
-        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
-    ) {
-        Column(
-            modifier = Modifier.padding(16.dp),
-            verticalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
-            Row (
-                modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically
-            ){
-                Text(
-                    text = mahasiswa.nama,
-                    style = MaterialTheme.typography.titleLarge,
-                )
-                Spacer(Modifier.weight(1f))
-                IconButton(onClick = {onDeleteClick(mahasiswa)}) {
-                    Icon(
-                        imageVector = Icons.Default.Delete,
-                        contentDescription = null,
-                    )
+class HomeViewModel (
+    private val repositoryMhs: RepositoryMhs
+) : ViewModel(){
+    var mhsUiState: HomeUiState by mutableStateOf(HomeUiState.Loading)
+        private set
+
+    init {
+        getMhs()
+    }
+
+    fun getMhs(){
+        viewModelScope.launch {
+            repositoryMhs.getAllMhs()
+                .onStart {
+                    mhsUiState = HomeUiState.Loading
                 }
-                Text(
-                    text = mahasiswa.nim,
-                    style = MaterialTheme.typography.titleMedium
-                )
-            }
-
-            Text(
-                text = mahasiswa.kelas,
-                style = MaterialTheme.typography.titleMedium
-            )
-            Text(
-                text = mahasiswa.alamat,
-                style = MaterialTheme.typography.titleMedium
-            )
+                .catch {
+                    mhsUiState = HomeUiState.Error(e = it)
+                }
+                .collect {
+                    mhsUiState = if (it.isEmpty()) {
+                        HomeUiState.Error(Exception("Belum ada data mahasiswa"))
+                    }else {
+                        HomeUiState.Success(it)
+                    }
+                }
         }
     }
-
-
-    @Composable
-    fun DeleteConfirmationDialog(
-        onDeleteConfirm: () -> Unit, onDeleteCancel: () -> Unit, modifier: Modifier = Modifier
-    ) {
-        AlertDialog(onDismissRequest = {/*Do Nothing*/ },
-            title = { Text("Delete Data") },
-            text = { Text("Apakah anda yakin ingin menghapus data?") },
-            modifier = modifier,
-            dismissButton = {
-                TextButton(onClick = onDeleteCancel) {
-                    Text(text = "Cancel")
-                }
-            },
-            confirmButton = {
-                TextButton(onClick = onDeleteConfirm) {
-                    Text(text = "Yes")
-                }
-            })
-    }
-
-}
