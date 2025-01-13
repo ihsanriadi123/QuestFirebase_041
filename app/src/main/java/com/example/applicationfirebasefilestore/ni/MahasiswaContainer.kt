f202857acaa116986476be890ef0b5e7f3c289c3
ihsanriadi123 <muhammad.ihsan.ft22@mail.umy.ac.id>
2025-01-13 08:36:09 +0700

    Membuat class mahasiswa dan di masukkan connect ke firebase nya

--- app/src/main/java/com/example/applicationfirebasefilestore/ni/MahasiswaContainer.kt
+++ app/src/main/java/com/example/applicationfirebasefilestore/ni/MahasiswaContainer.kt
@@ -8,3 +8,11 @@ import com.google.firebase.firestore.FirebaseFirestore
 interface AppContainer{
     val repositoryMhs: RepositoryMhs
 }
+
+
+class MahasiswaContainer: AppContainer{
+    private val firestore: FirebaseFirestore = FirebaseFirestore.getInstance()
+    override val repositoryMhs: RepositoryMhs by lazy {
+        NetworkRepositoryMhs(firestore)
+    }
+}
