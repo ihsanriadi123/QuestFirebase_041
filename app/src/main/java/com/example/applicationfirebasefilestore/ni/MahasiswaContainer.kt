18529875b00a25104057b9c658e9aa1cbe9642a4
ihsanriadi123 <muhammad.ihsan.ft22@mail.umy.ac.id>
2025-01-13 08:35:20 +0700

    Membuat Interface Container dengan menambahkan repository agar bisa mendetek kode yang di buat

--- app/src/main/java/com/example/applicationfirebasefilestore/ni/MahasiswaContainer.kt
+++ app/src/main/java/com/example/applicationfirebasefilestore/ni/MahasiswaContainer.kt
@@ -1,2 +1,10 @@
 package com.example.applicationfirebasefilestore.ni
 
+
+import com.example.applicationfirebasefilestore.Repository.RepositoryMhs
+import com.example.firebase.repository.NetworkRepositoryMhs
+import com.google.firebase.firestore.FirebaseFirestore
+
+interface AppContainer{
+    val repositoryMhs: RepositoryMhs
+}
