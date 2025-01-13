40b16bef2effe15784f417236fce0b1daff3f2d6
ihsanriadi123 <muhammad.ihsan.ft22@mail.umy.ac.id>
2025-01-13 08:38:40 +0700

    membuat kelas network nya setelah connect ke firebase nta bisa menambahkan data mahasiswa nya

--- app/src/main/java/com/example/applicationfirebasefilestore/Repository/NetworkRepositoryMhs.kt
+++ app/src/main/java/com/example/applicationfirebasefilestore/Repository/NetworkRepositoryMhs.kt
@@ -1,46 +1,77 @@
-package com.example.applicationfirebasefilestore.Repository
+package com.example.firebase.repository
 
+import com.example.applicationfirebasefilestore.Repository.RepositoryMhs
 import com.example.applicationfirebasefilestore.model.Mahasiswa
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.tasks.await
 
 class NetworkRepositoryMhs(
     private val firestore: FirebaseFirestore
 ) : RepositoryMhs {
     override suspend fun insertMhs(mahasiswa: Mahasiswa) {
-        TODO("Not yet implemented")
+        try {
+            firestore.collection("Mahasiswa").add(mahasiswa).await()
+
+        }catch (e: Exception){
+            throw Exception("Gagal menambahkan data mahasiswa:${e.message}")
+        }
     }
 
+
     override fun getAllMhs(): Flow<List<Mahasiswa>> = callbackFlow {
-        val mhsCollection = firestore.collection("Mahasiswa")
-            .orderBy("nim",Query.Direction.ASCENDING)
+        val mhsCollection =firestore.collection("Mahasiswa")
+            .orderBy("nim", Query.Direction.ASCENDING)
             .addSnapshotListener{ value, error ->
                 if (value != null){
                     val mhsList = value.documents.mapNotNull {
                         it.toObject(Mahasiswa::class.java)!!
                     }
-                    trySend(mhsList)
+                    trySend(mhsList) //try send memberikan fungsi untuk mengirim data ke flow
                 }
             }
-        awaitClose {
+        awaitClose{
             mhsCollection.remove()
         }
     }
 
-    override fun getMhs(nim: String): Flow<Mahasiswa> {
-        TODO("Not yet implemented")
+    override fun getMhs(nim: String): Flow<Mahasiswa> = callbackFlow{
+        val mhsDocument = firestore.collection("Mahasiswa")
+            .document(nim)
+            .addSnapshotListener{ value, error ->
+                if (value != null) {
+                    val mhs = value.toObject(Mahasiswa::class.java)!!
+                    trySend(mhs)
+                }
+            }
+        awaitClose{
+            mhsDocument.remove()
+        }
     }
 
     override suspend fun deleteMhs(mahasiswa: Mahasiswa) {
-        TODO("Not yet implemented")
+        try {
+            firestore.collection("Mahasiswa")
+                .document(mahasiswa.nim)
+                .delete()
+                .await()
+        } catch (e: Exception) {
+            throw Exception("Gagal menghapus data mahasiswa: ${e.message}")
+        }
     }
 
     override suspend fun updateMhs(mahasiswa: Mahasiswa) {
-        TODO("Not yet implemented")
+        try {
+            firestore.collection("Mahasiswa")
+                .document(mahasiswa.nim)
+                .set(mahasiswa)
+                .await()
+        }catch (e: Exception) {
+            throw Exception("Gagal mengupdate data mahasiswa: ${e.message}")
+        }
     }
 
 }
-
